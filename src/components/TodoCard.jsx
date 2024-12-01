9e45b55c2309ab657432c3f212d8cb4c4957dcb2
mostafa-gm51 <101727390+mostafa20021212@users.noreply.github.com>
2024-12-01 13:41:13 +0200

    Update TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -11,14 +11,12 @@ export default function TodoCard({
       <li className="todoItem">
         {todo}
         <div className="actionsContainer">
-          <i
-            className="fa-solid fa-pen-to-square"
-            onClick={() => handleEditTodo(index)}
-          ></i>
-          <i
-            className="fa-solid fa-trash"
-            onClick={() => handleDeteleTodo(index)}
-          ></i>
+          <button onClick={() => handleEditTodo(index)}>
+            <i className="fa-solid fa-pen-to-square"></i>
+          </button>
+          <button onClick={() => handleDeteleTodo(index)}>
+            <i className="fa-solid fa-trash"></i>
+          </button>
         </div>
       </li>
     </>
